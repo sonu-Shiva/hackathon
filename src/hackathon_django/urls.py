36c0e75dfa6491cf3b5e86fc3cd41730d0b27101
Satish Reddy <satish.kumar@galepartners.com>
2017-06-26 21:32:28 +0530

    add project and Home page is done

--- src/hackathon_django/urls.py
+++ src/hackathon_django/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('test_cases.urls', namespace="app")),
+    url(r'^qabot/', include('test_cases.urls', namespace="qabot")),
 ]
--- src/test_cases/views.py
+++ src/test_cases/views.py
@@ -3,14 +3,16 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Project, Reports
+from django.http import HttpResponseRedirect
 
 
 def project_view(request):
+    """Get all project's."""
     projects = Project.objects.all()
     context = {
         "projects": projects,
     }
-    return render(request, "projects_list.html", context)
+    return render(request, "index.html", context)
 
 
 def reports_view(request):
@@ -27,3 +29,11 @@ def render_report(request, report_id):
     except Reports.DoesNotExist:
         return HttpResponse(500)
     return HttpResponse(report)
+
+
+def add_project(request):
+    """Add new project."""
+    name = request.POST['project_name']
+    new_project = Project(name=name)
+    new_project.save()
+    return HttpResponseRedirect('/qabot')
