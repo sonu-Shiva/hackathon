ef8ce03e441eea9a248bc9c2b2f6eec467602e6b
J5 <jitendra.skkommu.cse11@iitbhu.ac.in>
2017-06-28 11:34:50 +0530

    Changing the code to use ModelForm

--- src/test_cases/forms.py
+++ src/test_cases/forms.py
@@ -1,7 +1,7 @@
 """Forms.py file."""
 
 from django import forms
-# from .models import Project, UseCase, Action, Reports
+from .models import Action
 
 
 ACTION_CHOICES = [
@@ -25,19 +25,21 @@ LOCATORS_CHOICES = [
 ]
 
 
-class ActionsFormset(forms.Form):
+class ActionsFormset(forms.ModelForm):
     """Actions form."""
 
     hidden_id = forms.CharField(required=False, widget=forms.HiddenInput())
     seq = forms.IntegerField(required=False, widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    use_case = forms.ChoiceField(required=False, widget=forms.HiddenInput())
-    description = forms.CharField(required=False)
     action = forms.ChoiceField(choices=ACTION_CHOICES, required=False)
     locators = forms.ChoiceField(choices=LOCATORS_CHOICES, required=False)
-    element_identifier = forms.CharField(required=False)
-    element_value = forms.CharField(required=False)
 
-    def __init__(self, use_case_choices=[], *args, **kwargs):
-        """Constructor Method."""
-        super(ActionsFormset, self).__init__(*args, **kwargs)
-        self.fields['use_case'] = forms.ChoiceField(choices=use_case_choices, required=False)
+    class Meta:
+        """Meta Data."""
+
+        model = Action
+        fields = ['seq', 'description', 'action', 'locators', 'element_identifier', 'element_value']
+
+    # def __init__(self, use_case_choices=[], *args, **kwargs):
+    #     """Constructor Method."""
+    #     super(ActionsFormset, self).__init__(*args, **kwargs)
+    #     self.fields['use_case'] = forms.ChoiceField(choices=use_case_choices, required=False)
