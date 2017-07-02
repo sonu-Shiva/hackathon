a16bb3d040af09bff7a27f093c391574ec5dfaa2
sandeeprajurs <sandeep.raju@galepartners.com>
2017-07-02 17:20:08 +0530

    deleted constants.java

--- java_src/src/com/test/automation/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.test.automation;
-
-public interface Constants 
-{
-	public static final String configPptPath = "C://Users//sandeepraju//Desktop//Workplace//QA-BOT//Config//config.properties";
-
-}
