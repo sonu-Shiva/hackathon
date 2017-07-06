3c48c6b9d16d921b02407aaa0a54e9dbce8cde1f
sandeeprajurs <sandeep.raju@galepartners.com>
2017-07-07 00:37:33 +0530

    added pom.xml and removed dependencies from build path

--- java_src/src/com/test/automation/Helper.java
+++ java_src/src/com/test/automation/Helper.java
@@ -4,10 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Helper implements Constants {
+public class Helper {
 	Connection c;
 	public Connection controller(){
-        System.out.println("in helper");
+   
 		try {
 		Class.forName(Property.getPropertyValue("DBDRIVER"));
 		} catch (ClassNotFoundException e1) {
@@ -15,9 +15,10 @@ public class Helper implements Constants {
 		e1.printStackTrace();
 		}
 		try {
+		
 		c = DriverManager.getConnection(Property.getPropertyValue("DBURL"),Property.getPropertyValue("DBUN"),Property.getPropertyValue("DBPWD"));
 		
-		System.out.println("connection successfully established "+c);
+		
 		}
 		catch (SQLException e) {
 		// TODO Auto-generated catch block
