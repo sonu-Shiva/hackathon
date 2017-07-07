3bf0448af747fdfd5cfe9a891443a181280e92b0
sandeeprajurs <sandeep.raju@galepartners.com>
2017-07-07 23:11:38 +0530

    handeling multiple usecase in jobid

--- Vagrantfile
+++ Vagrantfile
@@ -24,7 +24,7 @@ Vagrant.configure("2") do |config|
   # accessing "localhost:8080" will access port 80 on the guest machine.
   # NOTE: This will enable public access to the opened port
   config.vm.network "forwarded_port", guest: 8000, host: 9001
-  config.vm.network "forwarded_port", guest: 5432, host: 5432
+  config.vm.network "forwarded_port", guest: 5432, host: 5431
   config.vm.network "private_network", type: "dhcp"
 
   # Create a forwarded port mapping which allows access to a specific port
--- java_src/src/com/test/automation/Model.java
+++ java_src/src/com/test/automation/Model.java
@@ -8,7 +8,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -26,15 +28,18 @@ public synchronized static void triggerSelenium(String ucid,String browser){
 	
     Statement stmt=null;
     Statement stmt1=null;
+    Statement stmt2=null;
     PreparedStatement pStmt=null;
 	ResultSet rs=null;
 	ResultSet rs1=null;
+	ResultSet rs2=null;
 	String useCaseName=null;
 	WebDriver driver=null;
 	ExtentReports eReport = null;
 	ExtentTest testReport;
 	String dateVar = null;
 	String path = null;
+	List<String> ucidl=new ArrayList<String>();
 	int usecase_id = 0;
 	
 	//Browser triggereded on browser var value
@@ -56,14 +61,39 @@ public synchronized static void triggerSelenium(String ucid,String browser){
 	
 	//getting usecase id
 	String[] useCaseSplit=ucid.split("=");
-	String[] ucidl=useCaseSplit[1].split(",");
+	
+	if(useCaseSplit[0].equals("usecase_id")){
+		String[] stl=useCaseSplit[1].split(",");
+		 for(int i=0;i<stl.length;i++)
+			 ucidl.add(stl[i]);
+	}
+	
+	if(useCaseSplit[0].equals("job_id")){
+		   //get ucid's form job id and assign to ucidl variable
+		try {
+			stmt2=c.createStatement();
+			System.out.println(useCaseSplit[1]);
+	        String ucQuery="select usecase_id from qa_app_jobusecases where job_id="+useCaseSplit[1]+";";
+	        rs2=stmt2.executeQuery(ucQuery);
+	        int count=0;
+	        System.out.println(rs2);
+	        while(rs2.next()){
+	        	
+	        	System.out.println("usecase id: "+rs2.getString("usecase_id"));
+	        	ucidl.add(Integer.toString(rs2.getInt("usecase_id")));
+		    	count++;
+		    }
+		    } catch (SQLException e) {
+			   e.printStackTrace();
+		    }
+		}
 	
 	//no of usecaseid : no of times selenium performs actions
 	try {
 		for(String id:ucidl){
 			usecase_id=Integer.parseInt(id);
 		stmt1=c.createStatement();
-		String query1 = "SELECT use_case_name FROM test_cases_usecase Where id="+usecase_id+"; ";
+		String query1 = "SELECT use_case_name FROM qa_app_usecase Where id="+usecase_id+"; ";
 	    rs1=stmt1.executeQuery(query1);
 	   
 	    while(rs1.next()){
@@ -84,7 +114,7 @@ public synchronized static void triggerSelenium(String ucid,String browser){
 		testReport=eReport.startTest(useCaseName);
 		    
 	    	stmt=c.createStatement(); 
-	    	String query = "Select description,action,locators,element_identifier,element_value from test_cases_action where use_case_id="+usecase_id+" Order by seq";
+	    	String query = "Select description,action,locators,element_identifier,element_value from qa_app_action where use_case_id="+usecase_id+" Order by seq";
 	    	
 	    	rs = stmt.executeQuery(query);
 	    	
@@ -113,10 +143,11 @@ public synchronized static void triggerSelenium(String ucid,String browser){
 	}
 	 
      finally{
+    	 ucidl=null;
     	 try {
      		// inserting file pathe, usecase id and timstamp to db
     		 driver.close();
- 			 pStmt=c.prepareStatement("INSERT INTO test_cases_reports (report,use_case_id,time) VALUES (?,?,?)");
+ 			 pStmt=c.prepareStatement("INSERT INTO qa_app_reports (report,use_case_id,time) VALUES (?,?,?)");
  			 pStmt.setString(1,path);
  			 pStmt.setInt(2,usecase_id);
  			 pStmt.setString(3,dateVar); 
